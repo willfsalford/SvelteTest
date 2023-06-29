a16abe34a5e1cece91477abef871c825cbc824bc
willfsalford <w.f.forster@edu.salford.ac.uk>
2023-06-29 19:07:32 +0100

    3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,82 @@
-<h1>Welcome to SvelteKit</h1>
-<p>test 2</p>
+<script>
+    import testImg from "$lib/banner1.png"
+</script>
+<div class="cont">
+    <div class="">
+        <div class="tp banner">
+            <img src="images/transparent.PNG">
+        </div>
+        <div class="tpp banner">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="5000">
+                        <img src={testImg} class="d-block w-100" alt="banner1">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <img src="images/banner2.PNG" class="d-block w-100" alt="banner2">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <img src="images/banner3.PNG" class="d-block w-100" alt="banner2">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <img src="images/banner4.PNG" class="d-block w-100" alt="banner2">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <img src="images/banner5.PNG" class="d-block w-100" alt="banner2">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="line">
+            <img src="images/line_one.png">
+        </div>
+    </div>
+    <div id="homeC">
+        <div id="lLeft">
+            <img src="images/down-line.png">
+        </div>
+        <div id="video">
+            <video id="vid" autoplay="true" muted="muted" loop="true">
+                <source src="images/homepage.mp4" type="video/mp4"/>
+            </video>
+        </div>
+        <div id="about">
+            <h2>About the Artist</h2>
+            <p>
+                Trouser Project started in 2018 when I bought too many trousers from charity shops.
+                I started using bleach to add designs, and over the years have learnt many more up-cycling
+                techniques. I love to screen print, Lino print, cut & sew, hand paint, add patches.
+                Giving new life to old clothes - it’s sustainable and sexy and you’re going to be the only person
+                with this item of clothing.
+            </p>
+            <p>
+                I sell my work in Afflecks in Manchester alongside lots of other independent brands. Find our
+                collective shop Avant Garms on the second floor. I travel a lot for pop ups/ markets/ festivals so
+                follow me on instagram to keep updated. I also love working on custom requests, you can send
+                me old clothing of yours and ask for any design.
+            </p>
+        </div>
+        <div id="lRight">
+            <img src="images/down-line.png">
+        </div>
+    </div>
+</div>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<!-- <script>
+    window.addEventListener('DOMContentLoaded', (event) => {
+        var v = document.getElementById("vid");
+        v.play();
+    });
+</script> -->
