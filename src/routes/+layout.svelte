3c83bfc912c47dffe0dd59125863e9f3f3a5e9b3
willfsalford <w.f.forster@edu.salford.ac.uk>
2023-09-20 12:31:33 +0100

    4

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import tp_logo from "$lib/images/tp_logo.png"
+    import tp_trans from "$lib/images/transparent.png"
+</script>
+
+<div class="tp">
+    <div>
+        <img id="tp_banner" src={tp_trans} alt="logo2">
+    </div>
+</div>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,33 @@
 <script>
-    import testImg from "$lib/banner1.png"
+    import BImg1 from "$lib/images/banner1.png"
+    import BImg2 from "$lib/images/banner2.png"
+    import BImg3 from "$lib/images/banner3.png"
+    import BImg4 from "$lib/images/banner4.png"
+    import BImg5 from "$lib/images/banner5.png"
+    import line_one from "$lib/images/line_one.png"
+    import down_line from "$lib/images/down_line.png"
+    import hompage from "$lib/video/homepage.mp4"
+    
 </script>
 <div class="cont">
     <div class="">
-        <div class="tp banner">
-            <img src="images/transparent.PNG">
-        </div>
         <div class="tpp banner">
             <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active" data-bs-interval="5000">
-                        <img src={testImg} class="d-block w-100" alt="banner1">
+                        <img src={BImg1} class="d-block w-100" alt="banner1">
                     </div>
                     <div class="carousel-item" data-bs-interval="5000">
-                        <img src="images/banner2.PNG" class="d-block w-100" alt="banner2">
+                        <img src={BImg2} class="d-block w-100" alt="banner2">
                     </div>
                     <div class="carousel-item" data-bs-interval="5000">
-                        <img src="images/banner3.PNG" class="d-block w-100" alt="banner2">
+                        <img src={BImg3} class="d-block w-100" alt="banner3">
                     </div>
                     <div class="carousel-item" data-bs-interval="5000">
-                        <img src="images/banner4.PNG" class="d-block w-100" alt="banner2">
+                        <img src={BImg4} class="d-block w-100" alt="banner4">
                     </div>
                     <div class="carousel-item" data-bs-interval="5000">
-                        <img src="images/banner5.PNG" class="d-block w-100" alt="banner2">
+                        <img src={BImg5} class="d-block w-100" alt="banner5">
                     </div>
                 </div>
             </div>
@@ -30,36 +35,36 @@
     </div>
     <div>
         <div class="line">
-            <img src="images/line_one.png">
+            <img src={line_one} alt="line img">
         </div>
     </div>
     <div id="homeC">
         <div id="lLeft">
-            <img src="images/down-line.png">
+            <img src={down_line} alt="downline">
         </div>
         <div id="video">
             <video id="vid" autoplay="true" muted="muted" loop="true">
-                <source src="images/homepage.mp4" type="video/mp4"/>
+                <source src={hompage} type="video/mp4"/>
             </video>
         </div>
         <div id="about">
-            <h2>About the Artist</h2>
-            <p>
+            <textarea id="about-textarea" readonly>
+                About the Artist
                 Trouser Project started in 2018 when I bought too many trousers from charity shops.
                 I started using bleach to add designs, and over the years have learnt many more up-cycling
                 techniques. I love to screen print, Lino print, cut & sew, hand paint, add patches.
                 Giving new life to old clothes - it’s sustainable and sexy and you’re going to be the only person
                 with this item of clothing.
-            </p>
-            <p>
+
                 I sell my work in Afflecks in Manchester alongside lots of other independent brands. Find our
                 collective shop Avant Garms on the second floor. I travel a lot for pop ups/ markets/ festivals so
-                follow me on instagram to keep updated. I also love working on custom requests, you can send
+                follow me on Instagram to keep updated. I also love working on custom requests, you can send
                 me old clothing of yours and ask for any design.
-            </p>
+
+            </textarea>
         </div>
         <div id="lRight">
-            <img src="images/down-line.png">
+            <img src={down_line} alt="down line">
         </div>
     </div>
 </div>
